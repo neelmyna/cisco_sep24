0d604a05865b59770dde88caa7daf0b737b9a08e
Nithin NellakantaRao <mtd.nithin@gmail.com>
2024-09-20 14:12:40 +0530

    case study1 config file added

--- day5/netw/thread1.py
+++ day5/netw/thread1.py
@@ -1,11 +1,15 @@
 import threading
-import time
+import time as tm
+
+total = 0
 
 def print_numbers():
+    global total
     id = threading.get_ident()
-    for i in range(5):
-        print(f'{i}@{id}')
-        #time.sleep(0.025)
+    for i in range(1, 60000000):
+        #print(f'{i}@{id}')
+        total = total + i
+        #tm.sleep(0.025)
 
 threads = []
 for I in range(5):
@@ -15,36 +19,5 @@ for I in range(5):
 for I in range(5):    
     threads[I].join()   # Wait for the thread to finish
 
-print_numbers()
-
-""" import threading
-
-def print_numbers():
-    for i in range(5):
-        print(i)
-
-thread = threading.Thread(target=print_numbers)
-thread.start()  # Start the thread
-thread.join()   # Wait for the thread to finish """
-
-
-
-
-""" import time
-import threading
-
-def print_numbers():
-    thread_id = threading.get_ident()
-    for i in range(5):
-        print(f'@{thread_id}:{i}')
-        time.sleep(1)
-
-
-
-thread1 = threading.Thread(target=print_numbers)
-thread2 = threading.Thread(target=print_numbers)
-thread1.start()  # Start the thread
-thread2.start()  # Start the thread
-#thread1.join()   # Wait for the thread to finish
-#thread2.join()   # Wait for the thread to finish
-print_numbers() """
+#print_numbers()
+print(total)
